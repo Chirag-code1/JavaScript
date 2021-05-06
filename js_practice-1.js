f39722c4ded68f849677399e6737dbc7871fb78b
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-06 23:42:58 +0530

    Add files via upload

---
+++ js_practice-1.js
@@ -0,0 +1,100 @@
+// console.log("Hello World");
+
+// var myName ="chirag";
+// var $myAge=19;
+// console.log(myName+" "+$myAge);
+
+// // console.log(typeof($myAge));
+
+// console.log(10+"20"); //1020 ->bug=> number+string= concat.
+// console.log(10+20); //30
+// console.log("chirag"+" Independent Thinker"); //conat
+
+// console.log("chirag"-" Independent Thinker"); //NaN -> Not a Number.
+
+// console.log(true+true); //2
+// console.log(9-"4");
+
+
+// console.log(NaN===NaN); //false.
+// console.log(Number.NaN===NaN);
+
+//INCREMENT:
+//PREFIX Operators:
+// var num=15;
+// var myNum= ++num;
+// console.log(myNum);
+// console.log(num);
+
+
+// var num=15;
+// var myNum= ++num + 5;
+// console.log(myNum);
+// console.log(num);
+
+//POSTFIX Operators:
+// var num=15;
+// var myNum= num++;
+// console.log(myNum); // 15
+// console.log(num);   //16
+
+// var num=15;
+// var myNum= num++ + 5; 
+// console.log(myNum); // 20
+// console.log(num); //15
+
+//DECREMENT:
+//Postfix decrement operator::
+// var num=15;
+// var myNum= num-- + 5; //20
+// console.log(myNum); 
+// console.log(num);//14
+
+//Prefix decrement operator:
+// var num=15;
+// var myNum= --num + 5; //14+5=19
+// console.log(myNum); 
+// console.log(num);//14 
+
+//COMPARISON OPERATORS:
+// var a=30; 
+// var b=10;
+// console.log(a!=b);
+// console.log(a>=b);
+//etcetra, etc.
+
+//Logical Operators:
+//Logical &&:
+
+// var a=30; 
+// var b=-20;
+// console.log(a>b);   //True.
+// console.log(a>b && b>0);    //False.
+
+//Logical OR.
+// console.log((a<b) || (b>0) || (b>0)); //F || F || F = False
+
+//Logical NOT/Negation.
+// console.log(!((a<b) || (b>0) || (b>0))); // true.
+// console.log(!true);
+
+//string Operator: concatenation.
+
+// console.log("Hello "+ "World");
+// var myName= "chirag";
+// console.log(myName+" Khurana"+" Kapoor");
+
+// console.log(3**3); // 3*3*3
+// console.log(3**5); //3*3*3*3*3
+
+//Ques: Swap two variables without using third Variable:
+    //  var a=2;
+    //  var b=10;
+    // console.log("Enterred value of A:"+a);
+    // console.log("Enterred value of B:"+b);
+    //  a+=b;
+    //  b=a-b;
+    //  a-=b;
+    //  console.log("\nSwapped value of A:   "+a);
+    //  console.log("Swapped value of B:   "+b);
+   

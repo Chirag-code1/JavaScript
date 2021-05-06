06b00e1178c423773f8c97ea0f9c48a3ebf7c5ff
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-06 23:43:12 +0530

    Add files via upload

---
+++ js_practice-2.js
@@ -0,0 +1,107 @@
+//---------------------------------------------Section-4:----------------------------------------------
+    //-------------------------------------CONTROL STATEMENTS AND LOOPS:-----------------------------------
+    // If-else
+    // Switch statement
+    // While loops
+    // Do-while loops
+    // for loops
+    // for in loops
+    // for of loops
+    // Conditional (ternary) operators.
+
+    //---------------------------------------------------if-else:--------------------------------------
+
+    // var salary=1000;
+    // if(salary>1000){
+    //     salary+=300;
+    // }
+    // else if(salary<=1000) {
+    //     salary+=500;
+    // }
+    // console.log("Incremented Salary:"+salary);
+
+     //Ques: Check if a given year is a leap year or not? 
+    // var year=2024;
+    //    if(year%4==0){
+    //     console.log("Yes");
+    //    }
+    //    else if(year%400==0){
+    //     console.log("Yes");
+    //    }
+    //    else if(year%100==0){
+    //     console.log("No");
+    //    }
+
+//        if(year%4==0){
+//         if(year%100==0){
+//             if(year%400==0){
+// console.log("YES");
+//             }else{
+// console.log("NO");
+//             }
+//         }else{
+// console.log("YES");
+//         }
+//        }else{
+// console.log("NO");
+//        }
+
+
+//-------------------------------------------------------------Conditionl:-----------------------------------
+// var age=18;
+// console.log((age>=18)?"Can VOTE!":"Can't VOTE");
+
+//SWITCH Statement:
+// var area='circle';
+// var PI=3.142, l=5, b=4, r=3;
+
+// switch(area){
+//     case 'circle':
+//         console.log(PI*r*r);
+//         break;
+
+//     case 'square':
+//         console.log(4*l);
+//         break;
+
+//     case 'rectangle':
+//         console.log(l*b);
+//         break;
+        
+//     default:
+//         console.log("Enter valid data");
+// }
+
+//WHILE LOOP:
+ // var num =0;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+//DO WHILE LOOP:
+// var num=11;
+// do{
+//     debugger;
+//     console.log(num);
+//     num++;
+// }while(num<=10);
+
+//FOR LOOP:
+//var i=0;
+// for(i=0;i<=10;i++){
+//     debugger;
+//     console.log(i);
+// }
+
+//Printing tables of given num x:
+// var x=2,i=1;
+// for(i=1;i<=10;i++){
+//     console.log("2 X "+i+" = "+x*i);
+// }
+
+// do{
+//     console.log("2 X "+i+" = "+x*i);
+//     i++;
+// }while(i<=100);
+

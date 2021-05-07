80bd385e3a9ea79b17c0217cf799e25a2923c400
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-05-07 23:25:21 +0530

    Add files via upload

---
+++ js_practice-4.js
@@ -0,0 +1,12 @@
+//--------------------------------ARRAYS-----------------------------------------------------
+
+var myFriends=["chirag", 19, "male", "true"]; //just writing square brackets is enough to tell js that you r using array.
+
+//Traversing an Array:
+
+for(let i=0;i<myFriends.length;i++){
+    console.log(myFriends[i]);
+}
+
+// Finding length of an array: array_name.length; 
+// and this is not a fuction coze it doesnt have (). Rather it is a property.

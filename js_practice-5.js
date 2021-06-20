8f54fe27fec3c99da65ef86251783b9470a0cb70
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-06-20 11:09:05 +0530

    Create js_practice-5.js

---
+++ js_practice-5.js
@@ -0,0 +1,26 @@
+//Push Methods
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// const count =animals.push('chicken');
+// console.log(count);
+// console.log(animals);
+
+//unshift method=== returns the length of new array and 
+// adds the element in the beginning of Array not in end unline push()
+
+// const animals = ['pigs', 'goats', 'sheep'];
+
+// const count =animals.unshift('chicken');
+// console.log(count);
+// console.log(animals);
+
+//Example of Numbers in Array.
+
+// const myNum= [1,2,3,5];
+// myNum.unshift(4,6);
+// myNum.push(4,6);
+// console.log(myNum); 
+// Op=>[
+//     4, 6, 1, 2,
+//     3, 5, 4, 6
+//   ]

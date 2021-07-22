7ed1da85dac9e4569aba7ceddb358328607257d6
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-07-22 14:30:37 +0530

    Create UniqueCharacter.js

---
+++ UniqueCharacter.js
@@ -0,0 +1,12 @@
+function isUniqueChars(str){
+
+  	var i, j;
+for(i= 0 ; i<str.length; i++){
+    // console.log("Value-"+i);
+    for(j = i+1; j<str.length; j++){
+        if(str[i] === str[j])
+        {
+            return false;
+        }
+    }
+}
